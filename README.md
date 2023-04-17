a1c4032dc1db6d568e4c5d2295ae2d295cba47d0
“QinisoMazibuko  git config --global user.name “QinisoMazibuko <quequinton22@gmail.com>
2023-04-17 14:46:45 +0200

    initial setup

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align=center><strong>FastAPI Backend Example API</strong></h1>
+
+This is a sample repository aimed at demonstrating how we can build back-end services & APIs with FastAPI as well as assessing the benefits and limitations of the Framework! This template utilizes the following tech stack:
+
+- 🐍 [FastAPI](https://fastapi.tiangolo.com/)
+- 🐳 [Dockerized](https://www.docker.com/)
+- [Microsoft SQL Server](https://learn.microsoft.com/en-us/sql/linux/quickstart-install-connect-docker?view=sql-server-ver16&pivots=cs1-bash)
---
+++ src/main.py
@@ -0,0 +1,28 @@
+import fastapi
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def initialize_backend_application() -> fastapi.FastAPI:
+    app = fastapi.FastAPI()
+
+    #  add middleware eg. cors
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    #  add default route
+    @app.get('/')
+    def home():
+        return {"message": 'Server is running...'}
+
+    #  add enpoints
+    #  app.include_router(endpoints.router)
+
+    return app
+
+
+api: fastapi.FastAPI = initialize_backend_application()
