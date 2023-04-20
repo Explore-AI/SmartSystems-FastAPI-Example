a8d907a5b2437647169dda1fb201db97512e46eb
“QinisoMazibuko  git config --global user.name “QinisoMazibuko <quequinton22@gamail.com>
2023-04-20 12:43:56 +0200

    update databse connection properties

--- src/repositories/database.py
+++ src/repositories/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from urllib.parse import quote_plus
 
-#connect to a database server 
-SQLALCHEMY_DATABASE_URL = "mssql+pymssql://IdentityAdmin:%s@smart-systems.database.windows.net:1433/Identity_db"%quote_plus("PReplaceme@12")
+#Remote Azure DbServer: SQLALCHEMY_DATABASE_URL = "mssql+pymssql://IdentityAdmin:%s@smart-systems.database.windows.net:1433/Identity_db"%quote_plus("PReplaceme@12")
+SQLALCHEMY_DATABASE_URL = "mssql+pymssql://sa:%s@mssql:1433/Identity_DB"%quote_plus("Passw@rd")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
